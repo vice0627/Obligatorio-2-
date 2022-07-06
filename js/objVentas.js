ff4480d8879becaa7567013fb8cc48736d9d9ba7
vice0627 <vicenzo.carro.c@gmail.com>
2022-07-06 00:14:57 -0300

    todo listo menos estadísticas

--- js/objVentas.js
+++ js/objVentas.js
@@ -19,9 +19,9 @@ const venta = {
 
         const id = document.getElementById("id").value;
         const fecha = document.getElementById("fecha").value;
-        const cliente = document.getElementById("cliente");
-        const vendedor = document.getElementById("vendedor");
-        const vehiculo = document.getElementById("vehiculo");
+        const cliente = document.getElementById("clientes").value;
+        const vendedor = document.getElementById("vendedoresVenta").value;
+        const vehiculo = document.getElementById("vehiculosEnVenta").value;
 
   if(id == "" || cliente == "" || vendedor == "" || vehiculo == ""){
    swal("Faltan datos.");
@@ -41,6 +41,46 @@ else{
         }
 }
     },
+    baja: function(){
+        const id = document.getElementById('id').value;
+        const posicion = this.buscarPosicion(id);
+        if(posicion < 0){
+            swal("Error el id de la venta no existe.");
+        }else{
+            this.ventas.splice(posicion, 1);
+            this.reset();
+            this.listar();
+            swal("Venta eliminada con exito.");
+            memoria.escribir('ventas', this.ventas);
+        }
+    },
+    modificar : function (){
+        const id = document.getElementById('id').value;
+        const indice = this.buscarPosicion(id);
+        
+        if(indice > -1){
+
+            const venta = this.ventas[indice];
+
+            const fecha = document.getElementById("fecha").value;
+            const cliente = document.getElementById("clientes").value;
+            const vendedor = document.getElementById("vendedoresVenta").value;
+            const vehiculo = document.getElementById("vehiculosEnVenta").value;
+          
+            venta.Cliente = cliente;
+            venta.Vendedor = vendedor;
+            venta.Vehiculo = vehiculo;
+            venta.Fecha = fecha;
+
+            this.reset();
+            this.listar();
+            swal("Venta modificada con exito!!");
+            memoria.escribir('ventas', this.ventas);
+        }
+        else{
+            swal("Error el id de la venta no existe.");
+        }
+     },
     buscarPosicion: function (id) {
         for (let posicion = 0; posicion < this.ventas.length; posicion++) {
             const objVenta = this.ventas[posicion];
@@ -55,8 +95,10 @@ else{
         const lista = document.getElementById('listaVentas').options;
         lista.length = 0;
         for (const venta of this.ventas) {
-            const texto = "Id : " + venta.Id + '|Fecha : ' + venta.Fecha  + '|Cliente : ' +
-            venta.Cliente + '|Vendedor : ' + venta.Vendedor;
+            const vehiculo = document.getElementById("vehiculosEnVenta").value;
+            const marcaymodelo = vehiculo.split(" ");
+            const texto = "Id : " + venta.Id + ' |Fecha : ' + venta.Fecha  + ' |Cliente : ' +
+            venta.Cliente + ' |Vendedor : ' + venta.Vendedor + " |Marca : " + marcaymodelo[0] + " |Modelo : " + marcaymodelo[1];
             const elemento = new Option(texto);
             lista.add(elemento);
         }
@@ -65,7 +107,7 @@ else{
         const lista = document.getElementById('vehiculosEnVenta').options;
         lista.length = 0;
         for(const vehiculos of this.clonVehiculos){
-         const texto = vehiculos.Id;
+         const texto = vehiculos.Marca + " " + vehiculos.Modelo;
          const elemento = new Option(texto);
          lista.add(elemento);
      }
@@ -74,7 +116,7 @@ else{
         const lista = document.getElementById('clientes').options;
         lista.length = 0;
         for(const clientes of this.clonClientes){
-         const texto = clientes.Id;
+         const texto = clientes.Apellido + " " + clientes.Nombre;
          const elemento = new Option(texto);
          lista.add(elemento);
      }
@@ -83,7 +125,7 @@ else{
         const lista = document.getElementById('vendedoresVenta').options;
         lista.length = 0;
         for(const vendedores of this.clonVendedores){
-         const texto = vendedores.Id;
+         const texto = vendedores.Apellido + " " + vendedores.Nombre;
          const elemento = new Option(texto);
          lista.add(elemento);
      }
@@ -91,10 +133,6 @@ else{
     reset: function () {
         document.getElementById("id").value = "";
         document.getElementById("fecha").value = "";
-        document.getElementById("cliente").value = "";
-        document.getElementById("vendedor").value = "";
-        document.getElementById("vehiculo").value = "";
-      
      },
 
      inicializar: function () {
@@ -113,7 +151,7 @@ else{
      seleccionar: function(){
         const posicion = document.getElementById('listaVentas').selectedIndex;
         if(posicion < 0){
-           alert('Error en seleccionar');
+           swal('Error en seleccionar');
         }
         else{
           const objVentas = this.ventas[posicion];
